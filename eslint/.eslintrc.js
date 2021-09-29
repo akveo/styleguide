32a1a935a667806959ba0b0748bcf705b329affb
Ivan Vezhnavets <ivanvezhnavets@MacBook-Pro-Ivan.local>
2021-09-29 17:18:38 +0300

    fix: update react native config name

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -111,7 +111,7 @@ module.exports = {
 		},
 		{
 			files: [ '*.jsx', '*.tsx' ],
-			extends: [ './eslint_react_native.js' ], // react config path
+			extends: [ './eslint_react_native.js' ], // react native config path
 		}
 	]
 };
