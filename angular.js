e526c8a2e832ecf7485d2e15bee70114aa99a04b
Nikita1814 <kravchenko1814@gmail.com>
2022-05-24 12:14:53 +0300

    remove tabs from angular.js file

--- angular.js
+++ angular.js
@@ -1,16 +1,14 @@
 module.exports = {
-	extends: [
-
-	],
-	overrides: [
-		{
-			files: ['*.ts'],
-			extends: ['./rules/typescript', './rxjs', './angular/angular.eslintrc']
-		},
-		{
-			files: ['*.html'],
-			extends: ['./angular/angular-template.eslintrc']
-		},
-	],
-	rules: {},
+  extends: [],
+  overrides: [
+    {
+      files: ["*.ts"],
+      extends: ["./rules/typescript", "./rxjs", "./angular/angular.eslintrc"],
+    },
+    {
+      files: ["*.html"],
+      extends: ["./angular/angular-template.eslintrc"],
+    },
+  ],
+  rules: {},
 };
