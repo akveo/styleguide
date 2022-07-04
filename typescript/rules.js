f46863e6be7311a5b41c0986ac8c0a9f328f079a
mishkolesnikov <mishkolesnikov@gmail.com>
2022-07-04 17:20:24 +0400

    fix: proper use of ts-eslint extension rules

--- typescript/rules.js
+++ typescript/rules.js
@@ -7,7 +7,6 @@ const rules = {
   'semi-spacing': 'error',
   'comma-style': ['error', 'last'],
   'max-len': ['error', { code: 120 }],
-  'space-infix-ops': 'error',
   'space-unary-ops': 'error',
   'space-in-parens': ['error', 'never'],
   'no-new-wrappers': 'error',
@@ -15,10 +14,8 @@ const rules = {
   'no-new-func': 'error',
   'prefer-template': 'error',
   'no-useless-concat': 'error',
-  'no-array-constructor': 'error',
   'no-var': 'error',
   'prefer-const': 'error',
-  'no-throw-literal': 'error',
   'default-case': 'error',
   'eqeqeq': ['error', 'smart'],
   'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }],
@@ -45,24 +42,32 @@ const rules = {
   'no-new': 'error',
   'no-self-compare': 'error',
   'no-useless-call': 'error',
-  'func-call-spacing': 'error',
-
+  
   'key-spacing': 'error',
 
   'array-bracket-spacing': [
     'error',
     'never',
   ],
-  'object-curly-spacing': [
+
+  /* TS */
+  'space-infix-ops': 'off',
+  '@typescript-eslint/space-infix-ops': 'error',
+  'no-array-constructor': 'off',
+  '@typescript-eslint/no-array-constructor': 'error',
+  'no-throw-literal': 'off',
+  '@typescript-eslint/no-throw-literal': 'error',
+  'func-call-spacing': 'off',
+  '@typescript-eslint/func-call-spacing': 'error',
+  'object-curly-spacing': 'off',
+  '@typescript-eslint/object-curly-spacing': [
     'error',
     'always',
   ],
-  /* TS */
   '@typescript-eslint/prefer-readonly': 'off',
   '@typescript-eslint/prefer-for-of': 'error',
   '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
-
-  // TODO: add to styleguide exception regarding args starting with '_'
+  "no-unused-vars": "off",
   '@typescript-eslint/no-unused-vars': [
     'error',
     {
@@ -77,27 +82,33 @@ const rules = {
   '@typescript-eslint/await-thenable': 'error',
   '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
 
+  'brace-style': 'off',
   '@typescript-eslint/brace-style': ['error'],
 
+  'comma-spacing': 'off',
   '@typescript-eslint/comma-spacing': ['error'],
 
+  'keyword-spacing': 'off',
   '@typescript-eslint/keyword-spacing': ['error', { before: true }],
 
   '@typescript-eslint/member-delimiter-style': 'error',
 
   '@typescript-eslint/no-duplicate-imports': 'error',
 
+  'no-empty-function': 'off',
   '@typescript-eslint/no-empty-function': 'error',
 
   '@typescript-eslint/no-empty-interface': 'error',
   '@typescript-eslint/no-floating-promises': 'error',
 
+  'no-extra-semi': 'off',
   '@typescript-eslint/no-extra-semi': 'error',
 
   '@typescript-eslint/no-for-in-array': 'error',
   '@typescript-eslint/no-misused-promises': 'error',
   '@typescript-eslint/no-namespace': 'error',
   '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+  'no-redeclare': 'off',
   '@typescript-eslint/no-redeclare': 'error',
 
   '@typescript-eslint/no-unnecessary-type-constraint': 'error',
@@ -109,24 +120,25 @@ const rules = {
   '@typescript-eslint/prefer-reduce-type-parameter': 'error',
   '@typescript-eslint/prefer-string-starts-ends-with': 'error',
 
-
+  'semi': 'off',
   '@typescript-eslint/semi': ['error', 'always'],
 
   '@typescript-eslint/restrict-plus-operands': ['error', { checkCompoundAssignments: true }],
   '@typescript-eslint/type-annotation-spacing': 'error',
 
-
+  'indent': 'off',
   '@typescript-eslint/indent': ['error', 2],
 
+  'no-shadow': 'off',
   '@typescript-eslint/no-shadow': 'error',
 
-
+  'comma-dangle': 'off',
   '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
 
-
+  'quotes': 'off',
   '@typescript-eslint/quotes': ['error', 'single'],
 
-
+  'padding-line-between-statements': 'off',
   '@typescript-eslint/padding-line-between-statements': [
     'error',
     {
@@ -141,7 +153,7 @@ const rules = {
     },
   ],
 
-
+  'no-invalid-this': 'off',
   '@typescript-eslint/no-invalid-this': 'error',
 
   '@typescript-eslint/naming-convention': [
@@ -165,6 +177,7 @@ const rules = {
         'parameter',
       ],
       format: ['camelCase'],
+      leadingUnderscore: 'allow'
     },
     {
       selector: ['typeAlias'],
@@ -193,6 +206,7 @@ const rules = {
         'camelCase',
         'UPPER_CASE',
       ],
+      leadingUnderscore: 'allow'
     },
   ],
 };
@@ -201,7 +215,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
   ],
   rules,
 };
